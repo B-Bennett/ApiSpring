a01f1600afa58776e4bc2ca8247d4bd7623a5b64
BennettIronYard <bennettiy12@gmail.com>
2015-12-11 14:25:11 -0500

    almost

--- src/main/java/com/theironyard/ApiSpringController.java
+++ src/main/java/com/theironyard/ApiSpringController.java
@@ -1,6 +1,10 @@
 package com.theironyard;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 /**
  * Created by zach on 12/11/15.
@@ -8,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiSpringController {
     static final String SAMPLE_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+
+    @RequestMapping("/quote")
+    public HashMap getQuote() {
+        RestTemplate query = new RestTemplate();
+        HashMap result = query.getForObject(SAMPLE_URL, HashMap.class);
+        String type = (String) result.get("type");
+        if (type.equals("success")) {
+            HashMap value = (HashMap) result.get("value");
+            return value;
+        }
+        return result;
+    }
 }
